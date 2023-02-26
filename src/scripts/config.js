3b106b42a1ada8948be01b36fc128dde53b7c9b6
sven-gerlach <svengerlach@protonmail.com>
2023-02-25 23:58:05 -0500

    Amend config to accommodate API deployed on AWS Lambda

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,8 +2,8 @@
 
 export let apiUrl
 export const apiUrls = {
-  production: 'https://workout-tracker-api-cl81.onrender.com',
-  development: 'http://localhost:3001'
+  production: 'https://ck7k22n5j9.execute-api.us-east-1.amazonaws.com/prod',
+  development: 'http://localhost:3001/dev'
 }
 
 if (window.location.hostname === 'localhost') {
